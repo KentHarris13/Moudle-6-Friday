f79490ed2d03b06f4ebf495612dc643076f45235
Kent Harris <kentharris13@gmail.com>
2021-10-22 15:37:54 -0600

    as

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -21,17 +21,24 @@ test('I can start a game', async () => {
 });
 
 it('Testing upper left square', async () =>{
-    await driver.findElement(By.id('cell-0')).click()
+    const box= await driver.findElement(By.id('cell-0'))
+    await box.click()
+    expect(await box.getText()).toContain('X')
     await driver.sleep(5000)
     
 })
 
 it('Testing upper right square', async () => {
-    await driver.findElement(By.id('cell-2')).click()
+   const box= await driver.findElement(By.id('cell-2'))
+    await box.click()
     await driver.sleep(1000)
+    
 })
 
 it('Testing lower right square', async () => {
-    await driver.findElement(By.id('cell-8')).click()
+    const box= await driver.findElement(By.id('cell-8'))
+    await box.click()
     await driver.sleep(1000)
+    
 })
+
